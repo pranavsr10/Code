779a83c4141c3f6114ddf6486cfedd66607b2351
knightfall01 <darkvillain45gr@gmail.com>
2023-01-03 13:34:06 +0530

    commited now

--- Python-Projects/NewCode/superfunciton.py
+++ Python-Projects/NewCode/superfunciton.py
@@ -15,6 +15,17 @@ class Child(Person):
 
     def printshit(self):
         print("Hello My Name is " + self.FIRSTNAME + " " + self.SECONDNAME + "and I am " + self.DATE)
+
+class childofchild(Child):
+    def __init__ (self , fname , sname , date , randomnumber):
+        Child.__init__ (self , fname , sname , date)
+        self.name1 = fname
+        self.name2 = sname
+        self.dAte = date
+        self.Randomnumber = randomnumber
+
+    def printingsomething(self):
+        print("Hello there My name is " + self.name1 + " " + self.name2 + " and I am" + self.dAte + "My random number is " + self.Randomnumber)
 x = input("Enter your First Name :")
 y = input("Enter your Second Name :")
 object = Person(x,y)
@@ -22,3 +33,6 @@ object.helloworld()
 z = "18"
 childclass = Child(x,y,z)
 childclass.printshit()
+a = input("Enter your Random number :")
+grandchildclass = childofchild(x,y,z,a)
+grandchildclass.printingsomething()

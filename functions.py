a62af129874bb84c12d5c947b557fb44fe66239a
knightfall01 <darkvillain45gr@gmail.com>
2023-01-22 09:53:10 +0530

    Add existing project files to Git

--- functions.py
+++ functions.py
@@ -1,24 +1,24 @@
-# def function1(*y):
-#     for x in y:
-#         print(x)
-#
-# z = int(input("Enter the numbers:"))
-# y = int(input("Enter the numbers:"))
-# x = int(input("Enter the numbers:"))
-# a = int(input("Enter the numbers:"))
-# function1(z, y , x ,a )
-#
-# try:
-#     number = int(input("Enter the number:"))
-#     print (1/number)
-# except ValueError as error:
-#     print("Enter a number")
-#     print(error)
-# except ZeroDivisionError as error:
-#     print("Can't be divided by zero")
-#     print(error)
-# else:
-#     print("Nothing went wrong so it got executed sucessfully")
-# finally:
-#     print("The program has been executed.")
-#print('Hello') if 50 > 7 else print('Hi')
+def function1(*y):
+    for x in y:
+        print(x)
+
+z = int(input("Enter the numbers:"))
+y = int(input("Enter the numbers:"))
+x = int(input("Enter the numbers:"))
+a = int(input("Enter the numbers:"))
+function1(z, y , x ,a )
+
+try:
+    number = int(input("Enter the number:"))
+    print (1/number)
+except ValueError as error:
+    print("Enter a number")
+    print(error)
+except ZeroDivisionError as error:
+    print("Can't be divided by zero")
+    print(error)
+else:
+    print("Nothing went wrong so it got executed sucessfully")
+finally:
+    print("The program has been executed.")
+print('Hello') if 50 > 7 else print('Hi')

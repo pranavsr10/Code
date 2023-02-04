f04f4b4d88a8333f7861651ad3fc2cf6543e5873
knightfall01 <darkvillain45gr@gmail.com>
2023-02-04 21:31:52 +0530

    oof

--- Html/practice/practice.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("demo").innerHTML = 5 + 6;
-function myFunction() {
-  document.getElementById("demo1").innerHTML = "Hello Dolly!";
-  document.getElementById("demo2").innerHTML = "How are you?";
-}
-document.write(5 + 6);
-console.log(5 + 6);
-<button onclick="window.print()">Print this page</button>
-

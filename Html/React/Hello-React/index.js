7e5b34034f7fb8cd2bfeec7e2da213ba4b47a0a5
knightfall01 <darkvillain45gr@gmail.com>
2023-03-07 17:54:37 +0530

    React

--- Html/React/Hello-React/index.js
+++ Html/React/Hello-React/index.js
@@ -1,1 +1,12 @@
-ReactDom.render(<h1></h1>)
+ReactDOM.render(<ul> <li> HI</li> <li> Hello </li> <li> I am </li> </ul>, document.getElementById('root'));
+
+
+function Content(){
+    return(
+        <div>
+        <h1> This is the main content</h1>
+        </div>
+    )
+    }
+
+    ReactDOM.render(<Content />, document.getElementById('main'));

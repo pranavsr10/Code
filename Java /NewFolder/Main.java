564e131e8de93851ec3b46612118cc9ce006a378
knightfall01 <darkvillain45gr@gmail.com>
2023-03-16 20:32:04 +0530

    JAR

---
+++ Java /NewFolder/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Addition :");
+        int a=2;
+        int b=3;
+        int c=a+b;
+        System.out.println("The result is : "+c);
+        System.out.println("Subtraction :");
+        int d = a-b;
+        System.out.println("The result is : "+d);
+
+    }
+}

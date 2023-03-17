6512e5cb2123e82f47de9d6b3cf8f67c088a2ee8
knightfall01 <darkvillain45gr@gmail.com>
2023-03-17 22:53:24 +0530

    Web Added

---
+++ Java-Script/Scratch/index.js
@@ -0,0 +1,13 @@
+/* let person = {
+    name: 'knightfall',
+    age: 20,
+    interest: null
+}
+
+person.interest = "None";
+person['interest'] = "Nothing";
+console.log(person.interest);
+
+console.log(person.name); */
+
+ 

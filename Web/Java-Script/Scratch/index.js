af6bf710a9c94d8e02156e1126ce4f6f676ecea8
knightfall01 <darkvillain45gr@gmail.com>
2023-03-18 08:50:35 +0530

    Web Added

--- Web/Java-Script/Scratch/index.js
+++ Web/Java-Script/Scratch/index.js
@@ -9,5 +9,23 @@ person['interest'] = "Nothing";
 console.log(person.interest);
 
 console.log(person.name); */
+//
+// const agearray = [32,12,34,56,78,18];
+// const result = agearray.filter(thisfunction);
+// function thisfunction(agearray) {
+//   return age >= 18;
+// }
 
- 
+const ognumber = [ 1,2,3,4];
+const newnumber = ognumber.map(gomala)
+const result = ognumber.filter(filterfunction);
+function gomala(argums) {
+  return argums * 5;
+  
+}
+function filterfunction(argums) {
+  return argums >= 3 ;
+}
+
+console.log(newnumber);
+console.log(result);

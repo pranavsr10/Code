4c28f045abf5249c8098d790873eb710f8699a74
knightfall01 <darkvillain45gr@gmail.com>
2023-03-21 22:38:01 +0530

    Moved

---
+++ Web/TypeScript/Intro/tuple.ts
@@ -0,0 +1,16 @@
+let hiwer: [string, number]
+
+hiwer = ["user",1]
+
+let date: [number, string, number]
+
+date = [18, "march", 2023]
+
+type T = [number, string, number]
+
+let myVar: T = [18, "march", 2023]
+
+
+
+
+export {}

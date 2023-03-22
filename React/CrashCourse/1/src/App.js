74d70df3b353a9821eaa255d5fa7434b340787c8
knightfall01 <darkvillain45gr@gmail.com>
2023-03-22 20:11:05 +0530

    React

--- React/CrashCourse/1/src/App.js
+++ React/CrashCourse/1/src/App.js
@@ -2,16 +2,28 @@ import styles from './App.module.css';
 
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <div className={styles.textcolor}>
-      {/* <h1>Hello</h1> */}
-      <Print name="knightfall" desc="Hi there!" number={3786458}/>
-      <Print name="Darkest Knight" desc="Hello there" number={3786458}/>
-      </div>
+  const age = 23;
+  const isGreen = false;
+  const Condo = true;
+  // return <div className={styles.App}>{age >= 0 ? <h1>Valid age</h1> : <h1>Invalid age</h1> }</div>;
+  return <div className={styles.App}>
+    {age >= 0 ? <h1>Valid Age</h1> : <h1>Invalid Age</h1>}
+    <h1 style={{ color: isGreen ? "green" : "red" }}>Hello</h1>
+    <h1 style={{color: Condo ? "purple" : "black" }}>Hi</h1>
     </div>
-  );
+  // return (
+  //   <div className={styles.App}>
+  //     <div className={styles.textcolor}>
+  //     {/* <h1>Hello</h1> */}
+  //     <Print name="knightfall" desc="Hi there!" number={3786458}/>
+  //     <Print name="Darkest Knight" desc="Hello there" number={3786458}/>
+  //     </div>
+  //   </div>
+  // );
 }
+
+// const age: number = 23
+// age >= 10 ? console.log("This age is under 10 years") : console.log("This age is over 10 years");
 
 const Print = (props) => {
   return (

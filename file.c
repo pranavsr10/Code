55e23c7ee1c0fd3794ab592fb2c760f9c26ac77c
knightfall01 <darkvillain45gr@gmail.com>
2023-03-28 20:05:05 +0530

    Archcraft

--- file.c
+++ file.c
@@ -2,11 +2,12 @@
 int main(int argc, char *argv[])
 {
 int val = 0, n = 10;
+printf("Value before loop is %d\n", val);
 for(int i = 0; i < n; i++)
 {
 val = i;
 printf("%d \n", val );
 }
 printf("%d", val);
-  return 0;
+return 0;
 }
